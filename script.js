0ca319ec918ffd45fa0d3feee7bd8679da9b3895
DenysSergeev <sergeevdenys@gmail.com>
2022-07-04 13:59:24 +0300

    Практика, ч2. Применяем условия и циклы

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -8,11 +8,36 @@ let personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
+/* const a = prompt('Один из последних просмотренных фильмов?', '');
 const b = prompt('На сколько оцените его?', '');
 const c = prompt('Один из последних просмотренных фильмов?', '');
 const d = prompt('На сколько оцените его?', '');
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
+*/
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+    const b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотренно довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Ошибка");
+}
+
 console.log(personalMovieDB);
